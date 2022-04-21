19ac7956b215ee7d09bc37018e8f3e25497564aa
CodingStunts <jayhayes13@hotmail.co.uk>
2022-04-21 11:40:31 +0100

    added fonts for app title and general site font

--- src/components/StartUp.jsx
+++ src/components/StartUp.jsx
@@ -12,25 +12,27 @@ const StartUp = () => {
 
   return (
     <div className={styles.StartUp}>
-      <h2>Welcome to</h2>
-      <h1>Room with a Review!</h1>
-      <h3>A games review site.</h3>
-      <br></br>
-      <h4>Please log in to proceed to site</h4>
-      <form>
-        <select id="logIn" value={user} onChange={handleUserSelect}>
-          <option value="">*SELECT*</option>
-          <option value="tickle122">tickle122</option>
-          <option value="grumpy19">grumpy19</option>
-          <option value="happyamy2016">happyamy2016</option>
-          <option value="cooljmessy">cooljmessy</option>
-          <option value="weegembump">weegembump</option>
-          <option value="jessjelly">jessjelly</option>
-        </select>
-        <Link to="/home">
-          <button type="submit">Log in</button>
-        </Link>
-      </form>
+      <div className={styles.StartUpForm}>
+        <h2>Welcome to</h2>
+        <h1 className={styles.Title}>Room with a Review!</h1>
+        <h3>A games review site.</h3>
+        <hr />
+        <h4>Please log in to proceed to site</h4>
+        <form>
+          <select id="logIn" value={user} onChange={handleUserSelect}>
+            <option value="">*SELECT*</option>
+            <option value="tickle122">tickle122</option>
+            <option value="grumpy19">grumpy19</option>
+            <option value="happyamy2016">happyamy2016</option>
+            <option value="cooljmessy">cooljmessy</option>
+            <option value="weegembump">weegembump</option>
+            <option value="jessjelly">jessjelly</option>
+          </select>
+          <Link to="/home">
+            <button type="submit">Log in</button>
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
